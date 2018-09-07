3e319884d43f40f85e2606fd174c2daaae29d255
yidi <yiyiyidididi@qq.com>
2018-09-07 18:46:30 +0800

    ss

--- spring-cloud-microservice-in-action/microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/TestConfiguration.java
+++ spring-cloud-microservice-in-action/microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/TestConfiguration.java
@@ -1,16 +1,18 @@
 package com.itmuch.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
 
 @Configuration
 @ExcludeFromComponentScan
 public class TestConfiguration {
-  //  @Autowired
-  //  IClientConfig config;
+    @Autowired
+    IClientConfig config;
 
   @Bean
   public IRule ribbonRule() {
